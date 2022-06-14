2a11de957987b0546b36e6a051e182d5792ef49d
Richard Gozali <richard.g@ocbcnisp.com>
2022-06-14 10:26:56 +0700

    Navigation Page 4

--- DayTwo/GreenViewController.swift
+++ DayTwo/GreenViewController.swift
@@ -14,5 +14,9 @@ class GreenViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func movePage4(_ sender: Any) {
+        let BlueVc =  UIStoryboard(name: "Main", bundle:nil).instantiateViewController(withIdentifier: "BlueViewController")
+        self.navigationController?.pushViewController(BlueVc, animated: true)
+    }
+    
 }
