d012b682b13f04f0358a5edbb3a0d13ed66b4fe7
rakaabadi <rakaabadisusilo@gmail.com>
2022-06-14 14:29:26 +0700

    add passing variable to next page

--- DayTwo/RedViewController.swift
+++ DayTwo/RedViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class RedViewController: UIViewController {
-
+    struct globalVariable{
+        static var saldo=""
+    }
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onNextTap(_ sender: Any) {
+        globalVariable.saldo = textField.text ?? ""
+        let next = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "YellowViewController")
+        
+        self.navigationController?.pushViewController(next, animated: true)
+    }
+    
 }
 
--- DayTwo/YellowViewController.swift
+++ DayTwo/YellowViewController.swift
@@ -9,8 +9,10 @@ import UIKit
 
 class YellowViewController: UIViewController {
 
+    @IBOutlet weak var labelText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        labelText.text=RedViewController.globalVariable.saldo
         // Do any additional setup after loading the view.
     }
 
