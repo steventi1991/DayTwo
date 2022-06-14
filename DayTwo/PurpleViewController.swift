b1f4e4dae822b2570466061b16d026df59dc85ca
Richard Gozali <richard.g@ocbcnisp.com>
2022-06-14 10:30:03 +0700

    Navigation Page 1

--- DayTwo/PurpleViewController.swift
+++ DayTwo/PurpleViewController.swift
@@ -13,6 +13,10 @@ class PurpleViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func movePage1(_ sender: Any) {
+        let BlueVc =  UIStoryboard(name: "Main", bundle:nil).instantiateViewController(withIdentifier: "RedViewController")
+        self.navigationController?.pushViewController(BlueVc, animated: true)
+    }
+    
 
 }
