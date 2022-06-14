356d1c9c0b14db0b5aa9b692ecf60affb0cff9de
evannathaniel <evan.nathaniel0308@gmail.com>
2022-06-14 10:17:40 +0700

    change button next

--- DayTwo/RedViewController.swift
+++ DayTwo/RedViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class RedViewController: UIViewController {
 
+    @IBAction func redButton(_ sender: Any) {
+        let yellowVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "YellowViewController")
+        self.navigationController?.pushViewController(yellowVC, animated: true)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
