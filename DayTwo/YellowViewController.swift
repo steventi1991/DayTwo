a891c1f3183f53b3f8995fe069236d5d08e593f8
rakaabadi <rakaabadisusilo@gmail.com>
2022-06-14 15:18:46 +0700

    remove globalvariable

--- DayTwo/YellowViewController.swift
+++ DayTwo/YellowViewController.swift
@@ -8,14 +8,11 @@
 import UIKit
 
 class YellowViewController: UIViewController {
-    struct globalVariable{
-        static var saldo=""
-    }
+   
     var saldoSegue=""
     @IBOutlet weak var labelText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        //labelText.text=globalVariable.saldo
         labelText.text=saldoSegue
         // Do any additional setup after loading the view.
     }
